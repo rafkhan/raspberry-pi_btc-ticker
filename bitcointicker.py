e6aaa64a700ef7cc81c2594658372533328c2a51
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-01-16 18:11:33 -0500

    Added code snippets [NOT A WORKING VERSION]

--- bitcointicker.py
+++ bitcointicker.py
@@ -12,20 +12,29 @@ import time
 lcd = Adafruit_CharLCDPlate()
 lcd.begin(16,1)
 
-def mtgox()
-
-data["return"]["last"]["display"]
-data = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
-
 while 1:
-	lcd.clear()
-	data = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
-	time = datetime.now().strftime( '%F %H:%M\n' )
-	lcd.message(time)
-	lcd.message( "BTC/USD: " + data["return"]["last"]["display"])
-	sleep(30)
+        lcd.clear()
+        data = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
+        time = datetime.now().strftime( '%F %H:%M\n' )
+        lcd.message(time)
+        lcd.message( "BTC/USD: " + data["return"]["last"]["display"])
+        sleep(30)
 
-mtgox_result = data["result"]
+##### Code Snippets
+# API call and selection
+mtgox = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
+mtgox_currentprice = mtgox["return"]["last"]["value"]
+mtgox_result = mtgox["result"]
+
+# Checks if price > 1000
+if (int(round(float(mtgox_currentprice))) > 1000):
+	over1000 = int(round(float(mtgox_currentprice)))
+
+#If result != "success", write error message
 if mtgox_result != "success":
 	print "fail"
 	lcd.message("MtGox API error")
+#####
+
+
+
