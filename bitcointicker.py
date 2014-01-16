b59e7c4412c04ff9cf32c81604e3b9558fb4940e
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-01-16 13:59:20 -0500

    Committed code to test

--- bitcointicker.py
+++ bitcointicker.py
@@ -12,16 +12,13 @@ import time
 lcd = Adafruit_CharLCDPlate()
 lcd.begin(16,1)
 
-#infiniteloop = 1
+file = open("debug.txt")
+
+
 while 1:
-  #Clears LCD
 	lcd.clear()
-  #Loads data from standard MtGox API
-	       #data = json.load(urllib2.urlopen('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'))
 	data = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
-	#Formats date in standard YYYY-MM-DD HH-SS format
-	lcd.message(datetime.now().strftime( '%F %H:%M\n'))
-	#Displays and formats BTC/USD price from mtgox API response
+	time = datetime.now().strftime( '%F %H:%M\n' )
+	lcd.message(time)
 	lcd.message( "BTC/USD: " + data["return"]["last"]["display"])
-  #Sleeps for 30s
 	sleep(30)
