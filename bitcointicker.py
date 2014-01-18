7996911ea325cd16631e9b11837f8cb244416bad
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-01-18 02:37:47 -0500

    3AM end of code sesh. Slightly modified and working, thanks to @rafkhan. I'm sleepy

--- bitcointicker.py
+++ bitcointicker.py
@@ -1,46 +1,39 @@
  #!/usr/bin/python
-
+ 
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 from subprocess import *
 from time import sleep, strftime
 from datetime import datetime
-
+ 
 import json
 import urllib2
 import time
-     
+
 lcd = Adafruit_CharLCDPlate()
 lcd.begin(16,1)
-
-##### API calls and selection
-
-##### Bitcoin (BTC) #####
-def API():
-	global BTC
-	BTC = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
-	sleep(30)
-BTC_currentprice = BTC["return"]["last"]["value"]
-BTC_result = BTC["result"]
-# >1000 rollover
-if (int(round(float(BTC_currentprice))) > 1000):
-    BTC_finalprice = int(round(float(mtgox_currentprice)))
-else:
-	BTC_finalprice = BTC_currentprice
-#Error handling
-if BTC_result != "success":
-	print "fail"
-	lcd.message("MtGox API error")
-# Function
+ 
 def BTC():
-    lcd.clear()
-    API()
-    time = datetime.now().strftime( '%F %H:%M\n' )
-    lcd.message(time)
-    lcd.message( "BTC/USD: " + "$" + BTC_finalprice)
-BTC()
+	mtgox = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
+	if mtgox["result"] != "success":
+		lcd.message("MtGox API call failed! :(")
+		raise Exception("MtGox API call failed!")
+ 
+	current_price = mtgox["return"]["last"]["value"]
 
+	# > $1000 rollover, Rounds/Converts to int
+	if (int(round(float(current_price))) > 1000):
+		final_price = int(round(float(current_price)))
+	else:
+		final_price = current_price
+	return final_price
 
-#Dogecoin (DOGE)
-#DOGE = json.load(urllib2.urlopen('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132'))
-#DOGE_currentprice = DOGE["markets"]["DOGE"]["lasttradeprice"]
-#DOGE_result = DOGE["success"]
+while 1:
+	lcd.clear()
+	#Calls BTC function, gets time and formats.
+	price = BTC()
+	time = datetime.now().strftime( '%F %H:%M\n' )
+	#Displays time on first line, BTC/USD rate on next line.
+	lcd.message(time)
+	lcd.message( "BTC/USD: " + "$" + price)
+	#Sleeps until next API call can be made. 
+	sleep(30)
